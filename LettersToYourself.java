5afe1ca51f950d3a720a011f61db3512fb00579c
hpatel14b <39507487+hpatel14b@users.noreply.github.com>
2018-05-28 14:43:14 -0400

    Add files via upload

---
+++ LettersToYourself.java
@@ -0,0 +1,22 @@
+package com.letterstoyourself;
+
+public class LettersToYourself {
+
+	public static void main(String[] args) {
+		
+		System.out.println("+---------------------------------------------------+");
+		System.out.println("|                                               ### |");
+		System.out.println("|                                               ### |");
+		System.out.println("|                                               ### |");
+		System.out.println("|                                                   |");
+		System.out.println("|                                                   |");
+		System.out.println("|                       Heena Patel                 |");
+		System.out.println("|                       12 Sanalwood Road           |");
+		System.out.println("|                       Newark, DE 97173            |");
+		System.out.println("|                                                   |");
+		System.out.println("+---------------------------------------------------+");
+		
+		
+	}
+
+}
